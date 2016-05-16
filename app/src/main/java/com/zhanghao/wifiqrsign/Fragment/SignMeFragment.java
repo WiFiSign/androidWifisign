4ff43b82fd5f6de19a5dfd7e54a5d8313ce78e75
Ljx <314041831@qq.com>
2016-05-16 18:08:35 +0800

    ljx

--- app/src/main/java/com/zhanghao/wifiqrsign/Fragment/SignMeFragment.java
+++ app/src/main/java/com/zhanghao/wifiqrsign/Fragment/SignMeFragment.java
@@ -3,19 +3,101 @@ package com.zhanghao.wifiqrsign.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.zhanghao.wifiqrsign.Bean.SignBean;
 import com.zhanghao.wifiqrsign.R;
+import com.zhanghao.wifiqrsign.utils.SharedPreHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by 张浩 on 2016/4/12.
  */
 public class SignMeFragment extends Fragment {
 
+    private String name;
+    @Bind(R.id.list)
+    ListView listView;
+    private List<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+
     @Override
-    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.test,null);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.signme, null);
+        ButterKnife.bind(this, view);
+        initView();
+        return view;
+    }
+
+
+    public void initView(){
+        SharedPreHelper sharedPreHelper = new SharedPreHelper(getContext());
+        name = sharedPreHelper.getName();
+        GetSign();
+
+    }
+
+
+    public void GetSign() {
+        BmobQuery<SignBean> query = new BmobQuery<SignBean>();
+        query.addWhereEqualTo("name", name);
+
+        query.findObjects(getContext(), new FindListener<SignBean>() {
+            @Override
+            public void onSuccess(List<SignBean> list) {
+                for (SignBean signBean : list) {
+                    String Name = signBean.getName();
+                    String Loc = signBean.getLocation();
+                    String Time = signBean.getCreatedAt();
+                    Log.v("Name:", Name);
+                    Log.v("Loc:", Loc);
+                    Log.v("Time", Time);
+                    data.add(map(Name, Loc, Time));
+                    String [] from = new String[] {"Name", "Location", "Time"};
+                    int [] to = new int[] {R.id.SignMe_name, R.id.SignMe_location, R.id.SignMe_time};
+                    SimpleAdapter adapter = new SimpleAdapter(getContext(), data,
+                            R.layout.list_item, from, to);
+                    listView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+
+            }
+        });
+    }
+
+    public HashMap<String, String> map(String Name, String Location, String Time) {
+
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("Name", Name);
+        map.put("Location", Location);
+        map.put("Time", Time);
+        return map;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+        EventBus.getDefault().unregister(this);
     }
 }
